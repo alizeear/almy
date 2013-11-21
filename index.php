e1bdcc2e2fdc9f30ca0cc22beafa67661b2c825e
bazaim <moiet_moi@hotmail.fr>
2013-11-21 13:50:06 +0100

    modif' github debut

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
-   session_start();
-   include "include/almyDb.class";
-   $db =  almyDb::getDB();  
-   
-   // $_SESSION['id'] = 0;
-   
-   
-   if((!isset($_SESSION['id'])) && ((!isset($_GET['do'])) || ($_GET['do'] != "login"))) {
-      include "controllers/show_c.php";
-      include "view/show_v.php";
-   }
-   else {
-      include "controllers/admin_c.php";
-      include "view/admin_v.php";
-   }
+	session_start();
+	include "include/almyDb.class";
+	$db =  almyDb::getDB();  
+	
+	// pour le dev, nous rend admin connecté
+	if(isset($_GET['dev']))
+		$_SESSION['id'] = 1;
+	
+	// si l'user est pas connecté, et qu'il n'y a pas de requette de connexion, la page d'accueil est affiché
+	if((!isset($_SESSION['id'])) && ((!isset($_GET['do'])) || ($_GET['do'] != "login"))) {
+		include "controllers/show_c.php";
+		include "view/show_v.php";
+	}
+	else {
+	
+		include "controllers/admin_c.php";
+		include "view/admin_v.php";
+	}
 ?>
