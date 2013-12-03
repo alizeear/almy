cc1a5897914fb26037119af2aa5b21f5ea38e792
alizeear <arn.alizee@gmail.com>
2013-12-03 21:09:37 +0100

    slider fonctionnel (reste précédent, stop/play, auto et placer les boutons)

--- script/almy.js
+++ script/almy.js
@@ -9,27 +9,38 @@
            		
         return this.each(function(){
 
-        	$(this).css(
-        		'width', defauts.width,
-                "position", "relative"
-        	);
-            
-        	$(this).find('img').css(
-        		'width', defauts.widthImage,
-                "position", "absolute"
-        	).click(function(){
-        		$(this).animate({
-        			height: '0px',
-        			width: '0px'
-        		}, 500);
+        	$(this).css({
+        		width: defauts.width,
+                position: 'relative'
         	});
-            $(this).find('#categoriesMiddle img:gt(0)').hide();
 
-            $(this).find('.navNext').click(function(){
-                var $imageSuivante = $('#categoriesMiddle img:visible').next('img');
-                if($imageSuivante.length<1) $imageSuivante = $("#categoriesMiddle img:first");
-                $("#categoriesMiddle img:visible").fadeOut();
-                $imageSuivante.fadeIn();
+            var $widthImageNext = $(this).find('#categoriesMiddle img:gt(0)').width(); // stock dans une variable la width de l'image qui arrive
+                $(this).find('.imgContainer').css({ // modifie la width de la div contenant l'image pour que l'image reste bien au centre
+                    width: $widthImageNext
+                }, 500);
+            
+        	$(this).find('img').css({
+        		width: defauts.widthImage,
+                position: 'absolute'
+        	})
+         //    .click(function(){
+        	// 	$(this).animate({
+        	// 		height: '0px',
+        	// 		width: '0px'
+        	// 	}, 500);
+        	// })
+            ;
+            $(this).find('#categoriesMiddle img:gt(0)').hide();// on cache toutes les images sauf la premiere
+
+            $(this).find('.navNext').click(function(){ // au clic sur next
+                var $imageSuivante = $('#categoriesMiddle img:visible').next('img'); // on stock la valeur de l'image suivante dans une variable
+                if($imageSuivante.length<1) $imageSuivante = $("#categoriesMiddle img:first"); // on test si on est pas à la fin de la liste d'image et au cas ou on retourne à la première
+                var $widthImageNext = $('#categoriesMiddle img:visible').width(); // stock dans une variable la width de l'image qui arrive
+                $('.imgContainer').animate({ // modifie la width de la div contenant l'image pour que l'image reste bien au centre
+                    width: $widthImageNext
+                }, 500);
+                $("#categoriesMiddle img:visible").stop().fadeOut('slow'); // on cache l'image actuelle
+                $imageSuivante.stop().fadeIn('slow'); // on affiche la nouvelle
                 return false;
             });
 
